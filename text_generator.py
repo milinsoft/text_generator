66c35f364b0bfbaa6571143ed82be4b7fb8a89ff
parseltongist <>
2021-09-18 13:28:31 +0200

    Stage 5 with 10 real-sentence generator

--- text_generator.py
+++ text_generator.py
@@ -1,13 +1,14 @@
+import re
+
 from nltk.tokenize import WhitespaceTokenizer
 import nltk
 from collections import Counter
 import random
 
-# file_content = open("/Users/aleksander/Downloads/corpus.txt", "r", encoding="utf-8").readlines()   # str
-
 
 def obtain_dataset_from_file():
     with open(input(), "r", encoding="utf-8") as file:
+    #with open("/Users/aleksander/Downloads/corpus.txt", "r", encoding="utf-8") as file:
         file_content = file.readlines()
     return "".join(file_content)
 
@@ -79,9 +80,51 @@ def generate_pseudosentence() -> "pseudo sentence from 10 tokens":
     print(sentence_str)
 
 
+def set_initial_head():
+    random_head = "".join(random.choices(list(markov_model_dict.keys())))
+    template = r"\A[A-Z][a-z]+$"
+    if re.match(template, random_head):
+        return random_head
+    else:
+        return set_initial_head()
+
+
+def lastword_check():
+    ...
+
+
+def generate_readlsentence():
+    # don't forget to rename variables
+    initial_head = set_initial_head()
+    sentence_list = [initial_head]
+
+    while True:
+        current_tail = sentence_list[-1]
+        potential_tails = [key for key in markov_model_dict[current_tail].keys()]
+        tails_weights = [weight for weight in markov_model_dict[current_tail].values()]
+        new_tail = "".join(random.choices(potential_tails, weights=tails_weights))
+        sentence_list.append(new_tail)
+
+        if len(sentence_list) >= 5:
+            lastword = "".join(sentence_list[-1])
+            template = r"[A-Za-z]+[\.?!]{1,}$"
+            if re.match(template, lastword):
+                break
+        else:
+            continue
+
+    sentence_str = " ".join(sentence_list)
+    print(sentence_str)
+
+
 def generate_pseudotext() -> "10 pseudo-sentences with 10 tokens each":
     for _ in range(10):
         generate_pseudosentence()
+
+
+def generate_realtext():
+    for _ in range(10):
+       generate_readlsentence()
 
 
 if __name__ == '__main__':
@@ -98,4 +141,4 @@ if __name__ == '__main__':
     for key in markov_model_dict:
         markov_model_dict[key] = Counter(markov_model_dict[key])
 
-    generate_pseudotext()
+    generate_realtext()
