b26356fe3967e82f6a6f5866fda2576b33870719
parseltongist <>
2021-09-18 13:42:55 +0200

    Stage 5 with 10 real-sentence generator. Old functions removed

--- text_generator.py
+++ text_generator.py
@@ -8,76 +8,9 @@ import random
 
 def obtain_dataset_from_file():
     with open(input(), "r", encoding="utf-8") as file:
-    #with open("/Users/aleksander/Downloads/corpus.txt", "r", encoding="utf-8") as file:
+    # with open("/Users/aleksander/Downloads/corpus.txt", "r", encoding="utf-8") as file:
         file_content = file.readlines()
     return "".join(file_content)
-
-
-def token_by_index() -> "requested token, error message, or index":
-    token_index = input()
-    try:
-        token_index = int(token_index)
-    except ValueError:
-        if isinstance(token_index, str):
-            if token_index == "exit":
-                exit()
-            else:
-                print("Type Error. Please input an integer.")
-                return token_by_index()
-    else:
-        if token_index > len(tokens):
-            print("Index Error. Please input an integer that is in the range of the corpus.")
-            return token_by_index()
-        else:
-            print(tokens[int(token_index)])
-
-
-def bigrams_by_index() -> "requested bigram's head and tail, error message, or index":
-    bigram_index = input()
-    try:
-        bigram_index = int(bigram_index)
-    except ValueError:
-        if isinstance(bigram_index, str):
-            if bigram_index == "exit":
-                exit()
-            else:
-                print("Type Error. Please input an integer.")
-                return bigrams_by_index()
-    else:
-        if bigram_index > len(bigrams_tuple):
-            print("Index Error. Please input an integer that is in the range of the corpus.")
-            return bigrams_by_index()
-        else:
-            head = bigrams_tuple[bigram_index][0]
-            tail = bigrams_tuple[bigram_index][1]
-            print(f"Head: {head}\tTail: {tail}")
-
-
-def markov_tails_by_head() -> "all the possible tails and their corresponding counts.":
-    markov_head = input()
-    if markov_head == "exit":
-        exit()
-    elif markov_head not in markov_model_dict:
-        print(f"Head: {markov_head}")
-        print("Key Error. The requested word is not in the model. Please input another word.")
-        return markov_tails_by_head()
-    else:
-        print(f"Head: {markov_head}")
-        for bigram_tail, counter in markov_model_dict[markov_head].items():
-            print(f"Tail: {bigram_tail}\tCount: {counter}")
-
-
-def generate_pseudosentence() -> "pseudo sentence from 10 tokens":
-    random_head = "".join(random.choices(list(markov_model_dict.keys())))
-    sentence_list = [random_head]
-    while len(sentence_list) != 10:
-        current_tail = sentence_list[-1]
-        potential_tails = [key for key in markov_model_dict[current_tail].keys()]
-        tails_weights = [weight for weight in markov_model_dict[current_tail].values()]
-        new_tail = "".join(random.choices(potential_tails, weights=tails_weights))
-        sentence_list.append(new_tail)
-    sentence_str = " ".join(sentence_list)
-    print(sentence_str)
 
 
 def set_initial_head():
@@ -89,11 +22,12 @@ def set_initial_head():
         return set_initial_head()
 
 
-def lastword_check():
-    ...
+def lastword_check(lastword):
+    template = r"[A-Za-z]+[\.?!]{1,}$"
+    return re.match(template, lastword)
 
 
-def generate_readlsentence():
+def generate_realsentence():
     # don't forget to rename variables
     initial_head = set_initial_head()
     sentence_list = [initial_head]
@@ -107,24 +41,17 @@ def generate_readlsentence():
 
         if len(sentence_list) >= 5:
             lastword = "".join(sentence_list[-1])
-            template = r"[A-Za-z]+[\.?!]{1,}$"
-            if re.match(template, lastword):
+            if lastword_check(lastword):
                 break
-        else:
-            continue
-
+            else:
+                continue
     sentence_str = " ".join(sentence_list)
     print(sentence_str)
 
 
-def generate_pseudotext() -> "10 pseudo-sentences with 10 tokens each":
-    for _ in range(10):
-        generate_pseudosentence()
-
-
 def generate_realtext():
     for _ in range(10):
-       generate_readlsentence()
+        generate_realsentence()
 
 
 if __name__ == '__main__':
